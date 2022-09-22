3101e215ebc2461112400a74965db165ceb93566
Marcos Scheunemann <marcos_verg@hotmail.com>
2022-09-22 18:27:59 -0300

    new file:   aplicacao/venv/pyvenv.cfg

---
+++ aplicacao/apps/receitas/models.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Receita(models.Model):
+    pessoa = models.ForeignKey(User, on_delete=models.CASCADE)
+    objects = models.Manager()
+    nome_receita = models.CharField(max_length=200)
+    ingredientes = models.TextField()
+    modo_preparo = models.TextField()
+    tempo_preparo = models.IntegerField()
+    rendimento = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    date_receita = models.DateTimeField(default=datetime.now, blank=True)
+    foto_receita = models.ImageField(upload_to="fotos/%d/%m/%Y/", blank=True)
+    publicada = models.BooleanField(default=False)
+    def __str__(self):
+        return self.nome_receita
